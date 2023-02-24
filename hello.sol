3639d2377209dfeeaf631d9e19a7b20f9bbb8e65
davidmansion <93778907+davidmansion@users.noreply.github.com>
2023-02-24 06:35:49 +0000

    Create hello.sol

---
+++ hello.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+
+//NOTES
+//---But one think you have to keep in mind while writing solidity smart contract
+//---Compiler version must be greater than 0.8.13 and less than 0.9.0;
+
+pragma solidity ^0.8.13;
+contract MyFirstContract{
+    //string public hey = "Hey Davey";
+    //uint256 public no = 4;
+
+    string public hey;
+    uint256 public no;
+
+    //constructor(string memory _hey, uint _no){
+        //hey = _hey;
+        //no = _no;
+
+    //}
+
+    function addInfo(string memory _hey, uint _no) public{
+        hey = _hey;
+        no = _no;
+    }
+
+}
